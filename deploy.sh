e3e9f2084d9657615568269f88de83eaecb09e6c
Bekker Stacks <50801771+bekkerstacks@users.noreply.github.com>
2019-06-29 22:22:33 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-MODE=${1-https}
+MODE=${MODE-https}
 
 if [ ${MODE} == "http" ] 
   then
     echo "deploying traefik stack in http mode"
     docker stack deploy -c docker-compose_http.yml proxy
-    echo "traefik ui is available at:"
+    echo "Traefik UI is available at:"
     echo "- http://traefik.${DOMAIN}"
   else
     echo "deploying traefik stack in https mode"
     docker stack deploy -c docker-compose.yml proxy
-    echo "traefik ui is available at:"
+    echo "Traefik UI is available at:"
     echo "- https://traefik.${DOMAIN}"
 fi
